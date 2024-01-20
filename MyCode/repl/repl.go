77b3ed2c2dbd1443a172de8c17ec3caff72e3a67
Aditya-Chowdhary <adityachowdhary04@gmail.com>
2024-01-21 03:04:39 +0530

    1.5 Introducing REPL

---
+++ MyCode/repl/repl.go
@@ -0,0 +1,31 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/Aditya-Chowdhary/Monkey-Interpreter/lexer"
+	"github.com/Aditya-Chowdhary/Monkey-Interpreter/token"
+)
+
+const PROMPT = ">>"
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Fprintf(out, "%+v\n", tok)
+		}
+	}
+}
