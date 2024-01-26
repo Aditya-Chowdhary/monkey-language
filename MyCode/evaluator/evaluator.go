f00ae0bbbac89951f224b5e86c34a369c2812522
Aditya-Chowdhary <adityachowdhary04@gmail.com>
2024-01-26 17:47:52 +0530

    3.5.1 Eval Integer Literals

---
+++ MyCode/evaluator/evaluator.go
@@ -0,0 +1,33 @@
+package evaluator
+
+import (
+	"github.com/Aditya-Chowdhary/Monkey-Interpreter/ast"
+	"github.com/Aditya-Chowdhary/Monkey-Interpreter/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	// Statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	// Expressions
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+
+	return result
+}
