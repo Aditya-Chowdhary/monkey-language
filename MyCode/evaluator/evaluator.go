04edc3901b6b333cb12242ab8a8d7dfe02503acb
Aditya-Chowdhary <adityachowdhary04@gmail.com>
2024-01-26 17:56:21 +0530

    3.5.4 Eval Boolean and Null

--- MyCode/evaluator/evaluator.go
+++ MyCode/evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"github.com/Aditya-Chowdhary/Monkey-Interpreter/object"
 )
 
+var (
+	NULL = &object.Null{}
+	TRUE = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	// Statements
@@ -17,6 +23,9 @@ func Eval(node ast.Node) object.Object {
 	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 
 	return nil
@@ -30,4 +39,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
