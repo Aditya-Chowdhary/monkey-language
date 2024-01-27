5352917b447a858fd2210d235ca7ff1bad13fcff
Aditya-Chowdhary <adityachowdhary04@gmail.com>
2024-01-27 18:03:02 +0530

    3.9 Eval let statements

--- MyCode/repl/repl.go
+++ MyCode/repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/Aditya-Chowdhary/Monkey-Interpreter/evaluator"
 	"github.com/Aditya-Chowdhary/Monkey-Interpreter/lexer"
+	"github.com/Aditya-Chowdhary/Monkey-Interpreter/object"
 	"github.com/Aditya-Chowdhary/Monkey-Interpreter/parser"
 )
 
@@ -14,6 +15,7 @@ const PROMPT = ">>"
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -32,7 +34,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
