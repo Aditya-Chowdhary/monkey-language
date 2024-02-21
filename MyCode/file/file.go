cd82eceb7d7c6e35371740be02f958bb24498237
Aditya-Chowdhary <adityachowdhary04@gmail.com>
2024-02-21 17:19:08 +0530

    Added support for execution of files

---
+++ MyCode/file/file.go
@@ -0,0 +1,66 @@
+package file
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Aditya-Chowdhary/Monkey-Interpreter/compiler"
+	"github.com/Aditya-Chowdhary/Monkey-Interpreter/lexer"
+	"github.com/Aditya-Chowdhary/Monkey-Interpreter/object"
+	"github.com/Aditya-Chowdhary/Monkey-Interpreter/parser"
+	"github.com/Aditya-Chowdhary/Monkey-Interpreter/vm"
+)
+
+func RunCode(fileCode string) {
+	symbolTable := compiler.NewSymbolTable()
+	for i, v := range object.Builtins {
+		symbolTable.DefineBuiltin(i, v.Name)
+	}
+
+	l := lexer.New(fileCode)
+	p := parser.New(l)
+
+	program := p.ParseProgram()
+	if len(p.Errors()) != 0 {
+		printParserErrors(p.Errors())
+		os.Exit(1)
+	}
+
+	comp := compiler.New()
+	err := comp.Compile(program)
+	if err != nil {
+		fmt.Printf("Woops! Compilation failed: \n %s\n", err)
+		os.Exit(1)
+	}
+
+	code := comp.Bytecode()
+
+	machine := vm.New(code)
+
+	err = machine.Run()
+	if err != nil {
+		fmt.Printf("Woops! Executing bytecode failed:\n %s\n", err)
+		os.Exit(1)
+	}
+}
+
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
+func printParserErrors(errors []string) {
+	fmt.Print(MONKEY_FACE)
+	fmt.Println("Whoops! We ran into some monkey business here!")
+	for _, msg := range errors {
+		fmt.Println("\t" + msg + "\n")
+	}
+}
--- MyCode/main.go
+++ MyCode/main.go
@@ -4,16 +4,36 @@ import (
 	"fmt"
 	"os"
 	"os/user"
+	"strings"
 
+	"github.com/Aditya-Chowdhary/Monkey-Interpreter/file"
 	"github.com/Aditya-Chowdhary/Monkey-Interpreter/repl"
 )
 
 func main() {
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
+	if len(os.Args) == 1 {
+		user, err := user.Current()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Hello %s! This is the Monkey Programming Language!\n", user.Username)
+		fmt.Printf("Feel free to type in commands!\n")
+		repl.Start(os.Stdin, os.Stdout)
+	} else if len(os.Args) == 2 {
+		fileName := os.Args[1]
+
+		fileContent, err := os.ReadFile(fileName)
+		if err != nil {
+			fmt.Println("Error: Invalid file name")
+			os.Exit(1)
+		}
+		if !strings.HasSuffix(fileName, ".mky") {
+			fmt.Println("Error: Invalid file name")
+			os.Exit(1)
+		}
+		file.RunCode(string(fileContent))
+	} else {
+		fmt.Println("Error: Too many arguments!")
+		fmt.Println(`Usage: "./monkey [file-path]"`)
 	}
-	fmt.Printf("Hello %s! This is the Monkey Programming Language!\n", user.Username)
-	fmt.Printf("Feel free to type in commands!\n")
-	repl.Start(os.Stdin, os.Stdout)
 }
